f207c09ca866340810e7ef26825b011619e9e80d
danielarjordao <danielarjordao@hotmail.com>
2023-12-29 15:07:27 +0000

    save

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -12,22 +12,6 @@
 
 #include "get_next_line.h"
 
-int	newline(t_list *list)
-{
-	int	i;
-
-	if (!list)
-		return (0);
-	i = 0;
-	while (list->content[i])
-	{
-		if (list->content[i] == '\n')
-			return (0);
-		i++;
-	}
-	return (i);
-}
-
 t_list	*ft_lstnew(void *content)
 {
 	t_list	*lstnew;
@@ -39,6 +23,16 @@ t_list	*ft_lstnew(void *content)
 	lstnew->next = 0;
 	return (lstnew);
 }
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	if (!lst)
+		return (0);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
+}
+
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*lstlast;
@@ -50,4 +44,13 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	}
 	lstlast = ft_lstlast(*lst);
 	lstlast->next = new;
+}
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	if (!dest && !src)
+		return (0);
+	while (n--)
+		((char *)dest)[n] = ((const char *)src)[n];
+	return (dest);
 }
